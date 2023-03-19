8faa91e61c7c81302e5921e3a8916282120a8cd3
foobarfarm <70107532+foobarfarm@users.noreply.github.com>
2023-03-19 14:25:15 +0000

    Add call to importSave to primary adapter and add function to window in index

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
 import { exportSave } from './use-cases/export-save/exportSave';
+import { importSave } from './use-cases/import-save/importSave';
 
 (window as any).exportSave = () => {
   (async () => {
     await exportSave();
+  })();
+};
+
+(window as any).importSave = () => {
+  (async () => {
+    await importSave();
   })();
 };
