93a641b4d9dc7983f6f62e725e74d647b65fda08
foobarfarm <70107532+foobarfarm@users.noreply.github.com>
2023-03-19 13:48:24 +0000

    Add return type for configureFakeSystmeTime

--- src/utils/configureFakeSystemTimeForJest.ts
+++ src/utils/configureFakeSystemTimeForJest.ts
@@ -1,4 +1,4 @@
-export const configureFakeSystemTimeForJest = () => {
+export const configureFakeSystemTimeForJest = (): { fakeDate: Date } => {
   const fakeDate = new Date('1981-06-09');
   jest.useFakeTimers().setSystemTime(fakeDate);
 
