9bcc1c034a4185bcae1731944efe741d33ae98c0
foobarfarm <70107532+foobarfarm@users.noreply.github.com>
2023-03-19 14:25:39 +0000

    Add logic to use case to prompt user to upload file and log out deserialised save contents

--- src/use-cases/import-save/importSave.ts
+++ src/use-cases/import-save/importSave.ts
@@ -1,6 +1,14 @@
-export const importSave = () => {
-  // Get primary key from game name
-  // Request serialised save contents from client machine
-  // Look up game in indexedDB
-  // Overwrite serialised contents in IndexedDB
+import { container } from '../../IOC/container';
+import { TYPES } from '../../IOC/types';
+
+export const importSave = async () => {
+  const id = 'myId';
+
+  const uploadFile = container.get<UploadFile>(TYPES.UploadFile);
+  const serialisedSave = await uploadFile();
+
+  const saveSerialiser = container.get<SaveSerialiser>(TYPES.SaveSerialiser);
+  const save = saveSerialiser.deserialise(serialisedSave);
+
+  console.log(save);
 };
