ee9b55cbc8646689edf8973ca4dd6464e3c5af04
foobarfarm <70107532+foobarfarm@users.noreply.github.com>
2023-03-20 12:32:43 +0000

    Use content script to inject export/import functionality into page, update readme, remove popup extension code

--- src/README.md
+++ src/README.md
@@ -1,2 +1,2 @@
-
-# Typescript module
+# Browser extension for exporting/importing saves on Vimm's lair online emulator
+Note: Only works with Super Mario RPG right now.
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,28 @@
 import { exportSave } from './use-cases/export-save/exportSave';
 import { importSave } from './use-cases/import-save/importSave';
 
-const exportSaveButton = document.getElementById('export-save-button');
-exportSaveButton.addEventListener(
-  'click',
-  () => {
-    (async () => {
-      await exportSave();
-    })();
-  },
-  false
+const gameHeading = document.querySelector(
+  '#main > div.innerMain > div > div.mainContent > h2'
 );
 
-const importSaveButton = document.getElementById('import-save-button');
-importSaveButton.addEventListener(
-  'click',
-  () => {
-    (async () => {
-      await importSave();
-    })();
-  },
-  false
-);
+const exportButton = document.createElement('button');
+exportButton.innerText = 'Export Save';
+exportButton.onclick = () => {
+  (async () => {
+    await exportSave();
+  })();
+};
+
+gameHeading.append(exportButton);
+
+const importButton = document.createElement('button');
+importButton.innerText = 'Import Save';
+importButton.onclick = () => {
+  (async () => {
+    await importSave();
+
+    location.reload();
+  })();
+};
+
+gameHeading.append(importButton);
